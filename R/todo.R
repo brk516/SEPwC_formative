a99477ffc0fd7d0816c631707c14a8011d942ffc
Amelia Noras <brk516@york.ac.uk>
2026-04-18 14:57:36 +0100

    Linted script

--- R/todo.R
+++ R/todo.R
@@ -1,11 +1,11 @@
 #!/usr/bin/env Rscript
-# install.packages("argparse") - add this line incase this package isnt already installed
-# if doesn't work add this: install.packages("argparse", repos = "https://cran.rstudio.com/")
+# install.packages("argparse") - add this if not installed
+# if not work: install.packages("argparse", repos = "https://cran.rstudio.com/")
 suppressPackageStartupMessages({
   library(argparse)
 })
-options('python_cmd'='C:/ProgramData/Anaconda3/python.exe')
-  
+options("python_cmd" = "C:/ProgramData/Anaconda3/python.exe")
+
 TASK_FILE <- ".tasks.txt" # nolint # the .tasks.txt is where the list is saved to
 
 add_task <- function(task_input) {
@@ -15,42 +15,42 @@ add_task <- function(task_input) {
   if (file.exists(TASK_FILE)) {
     tasks <- readLines(TASK_FILE)
   } else {
-    tasks <- character() # if there are no current tasks it'll start empty
+    tasks <- character()# if there are no current tasks it'll start empty
   }
-    tasks <- c(tasks, task_input) # this combines the new task with the list (thats what the c does)
-    writeLines(tasks, TASK_FILE) # this will save the updated list, so you can see the new item
-  }
-
-list_tasks <- function() { 
+  tasks <- c(tasks, task_input) # combines new task with list
+  writeLines(tasks, TASK_FILE) # saves updated list
+}
+list_tasks <- function() {
   if (file.exists(TASK_FILE)) {
-  tasks <- readLines(TASK_FILE)
+    tasks <- readLines(TASK_FILE)
   } else {
     return("") # this returns empty string if no file is found
-  } 
-  if (length(tasks) == 0) {
-    return("Empty") # in case there are empty files
   }
-  formatted_rows <- paste0(seq_along(tasks), '. ', tasks) #seq_along(tasks) adds the numbers before the items
-  final_string <- paste(formatted_rows, collapse = "\n") # collapses all rows into a single string
+  if (length(tasks) == 0) {
+    return("") # in case there are empty files
+  }
+  formatted_rows <- paste0(seq_along(tasks), ". ", tasks)
+  #seq_along(tasks) adds the numbers before the items
+  final_string <- paste(formatted_rows, collapse = "\n")
+  # collapses all rows into a single string
   return(final_string)
 }
-
-    
 remove_task <- function(index) {
   if (!file.exists(TASK_FILE)) {
-    stop() # the stop() abandons the function
-  return(NULL) # ! = NOT, so if tasks don't exist or there are no rows in the list then states that
-  } 
+    stop("Error: list is empty!") # the stop() abandons the function
+    return(NULL)
+  }
+  # ! = NOT, tasks doesn't exist/no rows in list
   tasks <- readLines(TASK_FILE)
   if (length(tasks) == 0) {
-    stop() # again stop() will make the test work
-  } 
-  if (index < 1 || index > length(tasks))
-  { stop() # should stop task if a wrong index is entered
-  }else {
-  tasks <- tasks[-index] # REMOVES TASK, it keeps everything except the item at the chosen index
-  # [] these show a specific subset of a table
-  writeLines(tasks, TASK_FILE)
+    stop("Error: list is empty!") # again stop() will make the test work
+  }
+  if (index < 1 || index > length(tasks)) {
+    stop("Error: invalid index!") # should stop task if a wrong index is entered
+  } else {
+    tasks <- tasks[-index] # REMOVES TASK
+    # [] these show a specific subset of a table
+    writeLines(tasks, TASK_FILE)
   }
 } #continue by testing this.
 
@@ -82,6 +82,5 @@ if (sys.nframe() == 0) {
                       help = "Remove a task by index")
 
   args <- parser$parse_args()
-  print(args)
   main(args)
 }
